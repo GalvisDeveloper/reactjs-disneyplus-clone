d37bf1a4d22d1530b5cc9f0abf5d16b321cf5835
GalvisDeveloper <galvisdeveloper@gmail.com>
2021-08-19 20:29:44 -0500

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Home = () => {
+  return (
+    <Container>
+      <h1>Home</h1>
+    </Container>
+  );
+};
+
+export default Home;
